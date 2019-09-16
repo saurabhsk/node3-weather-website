91922b154ca25d7b338c06601b5faa06d951b190
Saurabh Kumar <sauku128@in.ibm.com>
2019-09-17 01:03:37 +0530

    Added more features in forecast.js file

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,8 @@
 const req = require('request')
 
 
+
+
 const forecast = (latitude,longitude,callback)=>{
     const url = 'https://api.darksky.net/forecast/dcaab4fb221fac10440ef4a7cd82ad34/'+ latitude+ ',' + longitude +'?units=si'
      
@@ -10,7 +12,7 @@ const forecast = (latitude,longitude,callback)=>{
        }else if(body.error){
         callback('Unable to find location! Try search again!', undefined)
        }else{
-            callback(undefined,body.daily.data[0].summary +'It is currently '+ body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined,body.daily.data[0].summary +'It is currently '+ body.currently.temperature + ' degrees out. High today is ' + body.daily.data[0].temperatureHigh + ' with a low of '+ body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
        }
     })
 
